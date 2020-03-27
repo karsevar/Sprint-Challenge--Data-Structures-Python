d126f66a8f90037d477fddfb3e05c3ff0d9e3ac6
masonkarsevar <masonkarsevar@gmail.com>
2020-03-27 08:43:02 -0700

    finished building out binary tree

--- names/names.py
+++ names/names.py
@@ -12,6 +12,55 @@ f.close()
 
 duplicates = []  # Return the list of duplicates in this data structure
 
+# For my machine this naive solution runs at 9.84 seconds. The 
+# following solution can be described as a O(n^2) solution because of 
+# the nested for loops and that in the worst case the algorithm will 
+# have to look through each array twice.
+
+# The O(n log(n)) solution for this problem would be using a binary tree 
+# in place of the nested for loops. Though in the worst case this solution 
+# will have a time complexity of O(n * n) since it's efficiency is dependent on 
+# how balanced the binary tree is.
+
+# first build out a binary search tree with the methods contains and 
+# insert.
+
+class BinarySearchTree:
+    def __init__(self, value):
+        self.value = value 
+        self.left = None 
+        self.right = None 
+
+    def insert(self, value):
+        if value < self.value:
+            if self.left is None:
+                self.left = BinarySearchTree(value)
+            else:
+                self.left.insert(value) 
+        else:
+            if self.right is None:
+                self.right = BinarySearchTree(value) 
+            else: 
+                self.right.insert(value)
+
+    def contains(self, target):
+        if self.value == target:
+            return True 
+        else:
+            if target < self.value:
+                if self.left is None:
+                    return False 
+                else:
+                    return self.left.contains(target) 
+            else:
+                if self.right is None:
+                    return False 
+                else:
+                    return self.right.contains(target)
+
+
+
+
 # Replace the nested for loops below with your improvements
 for name_1 in names_1:
     for name_2 in names_2:
